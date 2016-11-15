843c49096d9d46346e4a01b159087e805b87a40d
Ramseyer, Abe <aramsey@ilstu.edu>
2016-11-15 15:34:00 -0600

    Initial Commit

---
+++ java/com/androidtutorialpoint/cardviewtutorial/CardFragment.java
@@ -0,0 +1,184 @@
+package com.androidtutorialpoint.cardviewtutorial;
+
+        import android.bluetooth.BluetoothAdapter;
+        import android.content.ContentResolver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.content.pm.ResolveInfo;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.CheckBox;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import java.util.ArrayList;
+        import java.util.List;
+
+
+public class CardFragment extends Fragment {
+
+    ArrayList<WonderModel> listitems = new ArrayList<>();
+    RecyclerView MyRecyclerView;
+    String Wonders[] = {"Question 1","Question 2","Question 3","Question 4","Question 5","Question 6","Question 7"};
+    RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initializeList();
+        getActivity().setTitle("7 Wonders of the Modern World");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_card, container, false);
+        MyRecyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        MyRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager MyLayoutManager = new LinearLayoutManager(getActivity());
+        MyLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        if (listitems.size() > 0 & MyRecyclerView != null) {
+            MyRecyclerView.setAdapter(new MyAdapter(listitems));
+        }
+        MyRecyclerView.setLayoutManager(MyLayoutManager);
+
+
+        MyRecyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(getContext(), MyRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        Log.i("card touched", "at position: " + position);
+                        CheckBox cb = (CheckBox)view.findViewById(R.id.cardCheckBox);
+                        if(cb.isChecked())
+                            cb.setChecked(false);
+                        else
+                            cb.setChecked(true);
+                    }
+
+                    @Override public void onLongItemClick(View view, int position) {
+                        // do whatever
+                    }
+                })
+        );
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        private ArrayList<WonderModel> list;
+
+        public MyAdapter(ArrayList<WonderModel> Data) {
+            list = Data;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent,int viewType) {
+            // create a new view
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycle_items, parent, false);
+            MyViewHolder holder = new MyViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, int position) {
+
+            holder.questionTypeText.setText(list.get(position).getCardName());
+
+        }
+
+       @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView questionTypeText;
+        public Button sendButton; //TODO remove sendButton from layout and code, has been replaced with fab2send in MainActivity.java
+
+
+        public MyViewHolder(View v) {
+            super(v);
+            questionTypeText = (TextView) v.findViewById(R.id.questionTypeText);
+            sendButton = (Button) v.findViewById(R.id.sendButton);
+
+
+            sendButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("blutooth", "bt button tapped");
+
+                    BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT, Wonders[0]); //TODO find a way to get the card that was tapped and access that part of the array
+
+                    final PackageManager pm = getActivity().getApplicationContext().getPackageManager();
+                    List<ResolveInfo> appsList = pm.queryIntentActivities(intent, 0);
+
+                    if(appsList.size() > 0){
+                        String packageName = null;
+                        String className = null;
+                        boolean found = false;
+
+                        for(int i = 0; i < appsList.size(); i++){
+                            // find bluetooth in the list of activities
+                            packageName = appsList.get(i).activityInfo.packageName;
+                            if(packageName.equals("com.android.bluetooth")){
+                                className = appsList.get(i).activityInfo.name;
+                                found = true;
+                                break;
+                            }
+                        }
+                        if(! found){
+                            Toast.makeText(getContext(), R.string.blu_notfound_inlist,
+                                    Toast.LENGTH_SHORT).show();
+                            getActivity().finish();
+                        }
+
+                        intent.setClassName(packageName, className);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+
+
+        }
+    }
+
+    public void initializeList() {
+        listitems.clear();
+
+        for(int i =0;i<7;i++){
+
+            WonderModel item = new WonderModel();
+            item.setCardName(Wonders[i]);
+            item.setIsfav(0);
+            item.setIsturned(0);
+            listitems.add(item);
+        }
+
+    }
+}
---
+++ java/com/androidtutorialpoint/cardviewtutorial/MainActivity.java
@@ -0,0 +1,98 @@
+package com.androidtutorialpoint.cardviewtutorial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fab, fab1, fab2;
+    private Boolean isFabOpen = false;
+    private Animation fab1_open, fab2_open, fab_close, rotate_forward, rotate_backward;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1edit);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2send);
+        fab1_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab1_open);
+        fab2_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab2_open);
+
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CardFragment();
+            ;
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+
+
+        }
+
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+
+    }
+
+    public void animateFab() {
+        if(isFabOpen) {
+            fab.startAnimation(rotate_backward);
+            fab2.startAnimation(fab_close);
+            fab1.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        }
+        else {
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab1_open);
+            fab2.startAnimation(fab2_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+            System.out.println("opened the big one");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                animateFab();
+                Log.d("aramsey", "Big fAB tapped");
+                break;
+            case R.id.fab1edit:
+                Log.d("aramsey", "fab 1 tapped");
+                animateFab();
+                //TODO create an activity/layout that can add a writtent question
+//                i = new Intent(this,CreateAlarmActivity.class);
+//                startActivity(i);
+                break;
+            case R.id.fab2send:
+                Log.d("aramsey", "fab 2 tapped");
+                animateFab();
+                //TODO port send code over from CardFragment.java
+//                i = new Intent(this,CreateTimerActivity.class);
+//                startActivity(i);
+                break;
+        }
+    }
+}
